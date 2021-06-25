9c353157045dc14a9b6082c96dd990a5e30626f7
Wayne <w.p.lam107@gmail.com>
2021-06-25 12:10:11 -0400

    modified db

--- restapi/jobs/models.py
+++ restapi/jobs/models.py
@@ -17,12 +17,16 @@ class Job(models.Model):
         FOLLOW_UP = "F", _("Follow Up")
         INTERVIEW = "I", _("Interview")
 
+    class CoverLetter(models.TextChoices):
+        YES = "Yes", _("Yes")
+        NO = "No", _("No")
+
     company = models.CharField(max_length=64, blank=False, default='')
     job_title = models.CharField(max_length=64, blank=False, default='')
-    job_level = models.TextField(max_length=64, choices=JobLevel.choices, default=JobLevel.NO_LEVEL)
+    job_level = models.CharField(max_length=16, choices=JobLevel.choices, default=JobLevel.NO_LEVEL)
     min_years = models.IntegerField(null=True, blank=True)
     date_posted = models.DateField()
     date_applied = models.DateField()
-    cover_letter = models.TextField(max_length=2500, blank=True)
+    cover_letter = models.CharField(max_length=16, choices=CoverLetter.choices, default=CoverLetter.NO)
     response = models.CharField(max_length=16, choices=Response.choices, default=Response.NO_RESPONSE)
     response_date = models.DateField(null=True, blank=True)
